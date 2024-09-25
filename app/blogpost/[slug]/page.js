dedcc67289efecadcfea157f397788b2250346c7
Nithin <nithin958595@gmail.com>
2024-09-26 02:53:25 +0530

    added gtag.js

--- app/blogpost/[slug]/page.js
+++ app/blogpost/[slug]/page.js
@@ -1,24 +1,32 @@
 import matter from "gray-matter";
 import fs from 'fs';
 import path from 'path';
-import rehypeDocument from 'rehype-document'
-import rehypeFormat from 'rehype-format'
-import rehypeStringify from 'rehype-stringify'
-import remarkParse from 'remark-parse'
-import remarkRehype from 'remark-rehype'
-import { unified } from 'unified'
+import rehypeDocument from 'rehype-document';
+import rehypeFormat from 'rehype-format';
+import rehypeStringify from 'rehype-stringify';
+import remarkParse from 'remark-parse';
+import remarkRehype from 'remark-rehype';
+import { unified } from 'unified';
 import rehypePrettyCode from "rehype-pretty-code";
 
+export async function generateStaticParams() {
+    const contentDir = path.join(process.cwd(), 'app', 'content');
+    const filenames = fs.readdirSync(contentDir);
 
-export default async function BlogPost({ params }) {
-    const filepath = path.join(process.cwd(), 'app', 'content', `${params.slug}.md`);
+    return filenames.map(filename => ({
+        slug: filename.replace(/\.md$/, '')
+    }));
+}
+
+async function getData(slug) {
+    const filepath = path.join(process.cwd(), 'app', 'content', `${slug}.md`);
 
     let fileContent;
     try {
         fileContent = fs.readFileSync(filepath, "utf-8");
     } catch (err) {
         console.error('Error reading file:', err);
-        return <div>File not found or an error occurred.</div>;
+        return null;
     }
 
     const { content, data } = matter(fileContent);
@@ -26,7 +34,7 @@ export default async function BlogPost({ params }) {
     const processor = unified()
         .use(remarkParse)
         .use(remarkRehype)
-        .use(rehypeDocument, { title: '👋🌍' })
+        .use(rehypeDocument, { title: data.title })
         .use(rehypeFormat)
         .use(rehypeStringify)
         .use(rehypePrettyCode, {
@@ -46,14 +54,33 @@ export default async function BlogPost({ params }) {
         });
 
     const htmlContent = (await processor.process(content)).toString();
-    
+
+    return {
+        content: htmlContent,
+        data
+    };
+}
+
+export default async function BlogPost({ params }) {
+    const { slug } = params;
+    const postData = await getData(slug);
+
+    if (!postData) {
+        return {
+            notFound: true
+        };
+    }
+
+    const { content, data } = postData;
+
     return (
         <div className="max-w-5xl m-2 mx-auto p-6 rounded-lg shadow-lg ">
             <h1 className="text-3xl font-bold mb-4">{data.title}</h1>
-            <p className="text-lg mb-2 ">Author:<strong className="italic">{data.author}</strong> </p>
-            <p className="text-lg mb-2 border-l-4 p-2" ><strong></strong>&quot; {data.description}&quot;</p>
+            <p className="text-lg mb-2">Author: <strong className="italic">{data.author}</strong></p>
+            <p className="text-lg mb-2 border-l-4 p-2"><strong></strong>&quot; {data.description}&quot;</p>
             <p className="text-lg mb-4"><strong>Date:</strong> {data.date}</p>
-            <div className="prose dark:prose-invert" style={{ lineHeight: '1.8' }} dangerouslySetInnerHTML={{ __html: htmlContent }}></div>
+            <div className="prose dark:prose-invert" style={{ lineHeight: '1.8' }} dangerouslySetInnerHTML={{ __html: content }}></div>
+            <script async src="/public/gtag.js"></script>
         </div>
     );
 }
---
+++ app/public/gtag.js
@@ -0,0 +1,18 @@
+// public/gtag.js
+export const GA_TRACKING_ID = 'G-E15HMCW56N';
+
+// Initialize Google Analytics
+export const pageview = (url) => {
+  window.gtag('config', GA_TRACKING_ID, {
+    page_path: url,
+  });
+};
+
+// Log specific events
+export const event = ({ action, category, label, value }) => {
+  window.gtag('event', action, {
+    event_category: category,
+    event_label: label,
+    value: value,
+  });
+};
